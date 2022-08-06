21824d8b76f61dad30c9ac2b616d6ca697ca38d7
Tehseen Afzal <51958490+tahseenafzal@users.noreply.github.com>
2022-08-06 13:49:57 +0500

    Create home.js
    Create api for post home data

---
+++ pages/api/home.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      const { image, title, description, price, guests, beds, baths } =
+        req.body;
+
+      const home = await prisma.home.create({
+        data: { image, title, description, price, guests, beds, baths },
+      });
+
+      res.status(200).json(home);
+    } catch (e) {
+      res.status(500).json({ message: "Some thing went wrong" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res
+      .status(405)
+      .json({ message: `HTTP method ${req.method} is not supported.` });
+  }
+};
+
+export default handler;
